6691a4747bf774663686cbded4c57b754467f074
joshuaisaact <65121061+joshuaisaact@users.noreply.github.com>
2024-06-17 07:38:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,16 +6,27 @@ $(document).ready(() => {
 
     function updateTally() {
         $('#tally').text(`Wins: ${wins} | Losses: ${losses}`)
-    }
+    };
 
     function scrollBackground() {
         $('.background').css({ left: '0%' }).animate({ left: '-100%' }, 20000, 'linear', scrollBackground);
-    }
+    };
+
+    function coinFlip(choice) {
+        if (result === choice) {
+            $('#result').text(`Congratulations, you won! The result was ${result}.`)
+            wins++;
+        } else {
+            $('#result').text(`Sorry, you lost! The result was ${result}.`)
+            losses++;
+        }
+        updateTally();
+        $('#flipCoin, #result').show();
+        $('#flipped, #heads, #tails').hide();
+    };
 
     scrollBackground();
     
-    
-
     $('#flipCoin').on('click', () => {
         result = Math.random() < 0.5 ? 'heads' : 'tails';
         $('#flipCoin, #result').hide()
@@ -23,30 +34,8 @@ $(document).ready(() => {
         $('#flipped, #heads, #tails').show();
     })
 
-    $('#tails').on('click', () => {
-        if (result === 'tails') {
-            $('#result').text(`Congratulations, you won! The result was ${result}.`)
-            wins++;
-        } else {
-            $('#result').text(`Sorry, you lost! The result was ${result}.`)
-            losses++;
-        }
-        updateTally();
-        $('#flipCoin, #result').show();
-        $('#flipped, #heads, #tails').hide();
-    })
+    $('#tails').on('click', () => coinFlip('tails'));
 
-    $('#heads').on('click', () => {
-        if (result === 'heads') {
-            $('#result').text(`Congratulations, you won! The result was ${result}.`)
-            wins++;
-        } else {
-            $('#result').text(`Sorry, you lost! The result was ${result}.`)
-            losses++;
-        }
-        updateTally();
-        $('#flipCoin, #result').show();
-        $('#flipped, #heads, #tails').hide();
-    });
+    $('#heads').on('click', () => coinFlip('heads'));
 
 });
